23d2337900068bf47e3cbbf13057b49d4e8a77b5
Emmanuel Balogun <80977358+DreXuri@users.noreply.github.com>
2023-08-10 13:43:22 +0100

    date picker proveider

---
+++ lib/screens/home/cobtrollers/date_controllers/date_provider.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'date_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dateStateHash() => r'b65304716c282da3dc8eeaf09bf9c43b6dc5db4e';
+
+/// See also [DateState].
+@ProviderFor(DateState)
+final dateStateProvider =
+    AutoDisposeNotifierProvider<DateState, String>.internal(
+  DateState.new,
+  name: r'dateStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$dateStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DateState = AutoDisposeNotifier<String>;
+String _$timeStartStateHash() => r'b6fb5e34ac3cc7bf4c4a014c23c820a0534e2bf9';
+
+/// See also [TimeStartState].
+@ProviderFor(TimeStartState)
+final timeStartStateProvider =
+    AutoDisposeNotifierProvider<TimeStartState, String>.internal(
+  TimeStartState.new,
+  name: r'timeStartStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$timeStartStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TimeStartState = AutoDisposeNotifier<String>;
+String _$timeEndStateHash() => r'cc1d1641b4880e7c9ea9577684be4a2a085fdd61';
+
+/// See also [TimeEndState].
+@ProviderFor(TimeEndState)
+final timeEndStateProvider =
+    AutoDisposeNotifierProvider<TimeEndState, String>.internal(
+  TimeEndState.new,
+  name: r'timeEndStateProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$timeEndStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TimeEndState = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
