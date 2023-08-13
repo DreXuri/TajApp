cfd90d3dd8feb58dc4442ec2e7698616e108aff1
Emmanuel Balogun <80977358+DreXuri@users.noreply.github.com>
2023-08-13 01:59:08 +0100

    width size box

--- lib/general/widgets/width_space.dart
+++ lib/general/widgets/width_space.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class WidthSpace extends StatelessWidget {
-  final double? width;
-  const WidthSpace( {
+  final double width;
+  const WidthSpace({
     super.key,
-    this.width,
+    this.width = 10,
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-     width: width ?? 10.h,
+      width: width.h,
     );
   }
 }
