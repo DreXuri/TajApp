9dab79621cf52a058c7fd8506ddaa6641659bd2b
Emmanuel Balogun <80977358+DreXuri@users.noreply.github.com>
2023-08-19 21:29:48 +0100

    for push notification

--- lib/general/helpers/notification_heper.dart
+++ lib/general/helpers/notification_heper.dart
@@ -1,7 +1,19 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rxdart/rxdart.dart';
 
 class NotificationHelper {
   final WidgetRef ref;
 
-  NotificationHelper({required  this.ref})
+  NotificationHelper({required this.ref});
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  String? selectedNotificationPayload;
+
+  final BehaviorSubject<String?> selectedNotificationSubject =
+      BehaviorSubject<String?>();
+
+  initializedNotification() async {}
 }
