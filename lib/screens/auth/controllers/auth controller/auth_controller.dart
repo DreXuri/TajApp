33b7faeac9e68708bdc662e64ed328c563b69691
Emmanuel Balogun <80977358+DreXuri@users.noreply.github.com>
2023-08-24 10:26:15 +0100

    auto control

--- lib/screens/auth/controllers/auth controller/auth_controller.dart
+++ lib/screens/auth/controllers/auth controller/auth_controller.dart
@@ -1,42 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:todoapp/general/core/error/failure.dart';
 import 'package:todoapp/general/helpers/db_helper.dart';
 import 'package:todoapp/general/models/user_model.dart';
+import 'package:todoapp/screens/auth/controllers/auth%20controller/registration_state.dart';
 import 'package:todoapp/screens/auth/repository/auth_requester.dart';
 
 // part 'auth_provider.g.dart';
 
-final authControllerProvider = Provider((ref) {
-  final authRepository = ref.watch(authRepositoryProvider);
-  return AuthController(authRepository: authRepository);
-});
+// final authControllerProvider = Provider
+// ((ref) {
+//   final authRepository = ref.watch(authRepositoryProvider);
 
-class AuthController {
+final authControllerProvider =
+    StateNotifierProvider<RegistrationStateNotifier, RegistrationState>(
+  (ref) => RegistrationStateNotifier(ref.read(authRepositoryProvider)),
+);
+
+//   return AuthController(authRepository: authRepository);
+// });
+
+// class AuthController {
+//   final AuthRepisotory authRepository;
+//   AuthController({required this.authRepository}) : super();
+class RegistrationStateNotifier extends StateNotifier<RegistrationState> {
   final AuthRepisotory authRepository;
-  AuthController({required this.authRepository}) : super();
+  RegistrationStateNotifier(this.authRepository)
+      : super(RegistrationState.initial());
 
-  void verifyOtCodep({
-    required BuildContext context,
-    required String smsId,
-    required String smsCode,
-    required bool isMounted,
-  }) {
-    authRepository.verifyOtp(
+  void verifyOtCodep(
+      {required BuildContext context,
+      required String smsId,
+      required String smsCode,
+      required bool isMounted}) async {
+    state = const RegistrationState.loading();
+    final result = await authRepository.verifyOtp(
       context: context,
       smsId: smsId,
       smsCode: smsCode,
       isMounted: isMounted,
     );
+    // result.fold(
+    //   (failure) => state = RegistrationState.error(
+    //     DisplayMessagesUtils.mapFailureToMessage(failure),
+    //   ),
+    //   (success) => state = RegistrationState.loaded(),
+    // );
+
+    // state = const RegistrationState.initial();
+    // authRepository.verifyOtp(
+    //   context: context,
+    //   smsId: smsId,
+    //   smsCode: smsCode,
+    //   isMounted: isMounted,
+    // );
   }
 
   void sendyOtp({
     required BuildContext context,
     required String phone,
-  }) {
-    authRepository.sendyOtp(
+  }) async {
+    state = const RegistrationState.loading();
+    final result = authRepository.sendyOtp(
       context: context,
       phone: phone,
     );
+    // result.fold(
+    //   (failure) => state = RegistrationState.error(
+    //     DisplayMessagesUtils.mapFailureToMessage(failure),
+    //   ),
+    //   (success) => state = RegistrationState.loaded(),
+    // );
+
+    // authRepository.sendyOtp(
+    //   context: context,
+    //   phone: phone,
+    // );
   }
 }
